584f2de1cfc5723a3d7d48fbc4711de9d3951cfa
Tufan Turkaslan <tufanturkaslan@gmail.com>
2012-09-03 10:45:19 +0300

    New one..

--- Movie/build/generated-sources/ap-source-output/com/prime/movies/domain/Movie_.java
+++ Movie/build/generated-sources/ap-source-output/com/prime/movies/domain/Movie_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2012-08-31T11:46:21")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2012-09-03T10:21:35")
 @StaticMetamodel(Movie.class)
 public class Movie_ { 
 
